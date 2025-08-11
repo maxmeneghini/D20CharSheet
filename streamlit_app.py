576b8979c4c1b74668f9352916de8654edaa7252
maxmeneghini <134298818+maxmeneghini@users.noreply.github.com>
2025-08-11 09:27:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -296,11 +296,6 @@ with s5:
     with st.container(border=True):
         labelled_number("Nonlethal", "nonlethal", st.session_state.get("nonlethal", 0), min_value=0)
 
-    st.markdown("**Nonlethal Damage**")
-    st.markdown("<div class='card'>", unsafe_allow_html=True)
-    labelled_number("Nonlethal", "nonlethal", st.session_state.get("nonlethal", 0), min_value=0)
-    st.markdown("</div>", unsafe_allow_html=True)
-
 st.markdown("---")
 lcol, mcol, rcol = st.columns([2.4, 3, 2.6])
 with lcol:
